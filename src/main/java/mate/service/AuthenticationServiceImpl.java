bf64f9e33bb8311894ac917bbaa1ddd069bd879c
Stas Shepet <milka33349@gmail.com>
2021-10-21 12:55:10 +0300

    added logger, added .gitignore, removed comments

--- src/main/java/mate/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/service/AuthenticationServiceImpl.java
@@ -5,14 +5,18 @@ import mate.exception.AuthenticationException;
 import mate.lib.Inject;
 import mate.lib.Service;
 import mate.model.Driver;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
+    private static final Logger logger = LogManager.getLogger(AuthenticationService.class);
     @Inject
     private DriverService driverService;
 
     @Override
     public Driver login(String login, String password) throws AuthenticationException {
+        logger.info("Method login was called. Params: login = {}", login);
         Optional<Driver> driver = driverService.findByLogin(login);
         if (driver.isEmpty() || !driver.get().getPassword().equals(password)) {
             throw new AuthenticationException("Login or password are incorrect!");
